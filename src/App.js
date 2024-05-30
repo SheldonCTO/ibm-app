01593a348e0120b00098e983db1cef3982ba349e
SheldonCTO <c.cto1981@gmail.com>
2024-05-30 00:01:56 -0400

    Notification

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar';
+import Notification from './Components/Notification/Notification';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+<div className="App">
         <BrowserRouter>
           <Notification>
-          <Navbar/>
               <Routes>
-                {/* <Route path="./Conponenets/Login/Login.js" element={<Login />}/>
-                <Route path="./Conponenets/Sign_Up/Sign_Up.js" element={<SignUp />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<SignUp />}/>
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
                  <Route path="<component_route>" element={<component_name/>}/> //replace the component_route with the component path  and component_name with the component name as imported in the **App.js file**. 
-                 */}
+                
               </Routes>
           </Notification>
         </BrowserRouter>
