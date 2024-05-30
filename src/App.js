a783044e2b970d584f7264466b9183b49d4adc2e
SheldonCTO <c.cto1981@gmail.com>
2024-05-30 13:24:14 -0400

    fixbug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Notification from './Components/Notification/Notification';
-import Layout from './Components/Landing_Page/Landing_page';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Landing_Page from './Components/LandingPage/Landing_Page';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+
 import GiveReviews from './Components/ReviewForm/ReviewForm.js';
-import Navbar from './Components/Navbar/Navbar';
+// import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
 <div className="App">
         <BrowserRouter>
-        <Navbar/>
+        {/* <Navbar/> */}
           <Notification>
               <Routes>
-                <Route path="/login" element={<Login />}/>
-                <Route path="/signup" element={<SignUp />}/>
-                <Route path="/" element={<Landing_Page/>}/>
-                <Route path="/instant-consultation" element={<InstantConsultation />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/sign_up" element={<Sign_Up />} />
+              <Route path="/landing_page" element={<Landing_Page />} />
+              <Route path="/instant_consultation" element={<InstantConsultation />} />
                  <Route path="<component_route>" element={<component_name/>}/> //replace the component_route with the component path  and component_name with the component name as imported in the **App.js file**. 
                 
               </Routes>
