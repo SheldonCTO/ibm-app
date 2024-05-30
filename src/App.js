695f4a78df1c2a106141624725109affd8d2e5a5
SheldonCTO <c.cto1981@gmail.com>
2024-05-30 11:26:12 -0400

    booking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Notification from './Components/Notification/Notification';
+import Layout from './Components/Landing_Page/Landing_page'};
 import GiveReviews from './Components/ReviewForm/ReviewForm.js';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
               <Routes>
                 <Route path="/login" element={<Login />}/>
                 <Route path="/signup" element={<SignUp />}/>
+                <Route path="/" element={<Landing_Page/>}/>
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
                  <Route path="<component_route>" element={<component_name/>}/> //replace the component_route with the component path  and component_name with the component name as imported in the **App.js file**. 
                 
--- src/Conponents/Navbar/Navbar.js
+++ src/Conponents/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import &#39;./Navbar.css&#39;
 import "./Navbar.css";
 
 
