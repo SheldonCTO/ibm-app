2e9ac83d51ddc1f82805d77aed2b00443c703e51
SheldonCTO <c.cto1981@gmail.com>
2024-05-29 23:39:20 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-<div className="App">
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
         <BrowserRouter>
           <Notification>
+          <Navbar/>
               <Routes>
-                <Route path="/login" element={<Login />}/>
-                <Route path="/signup" element={<SignUp />}/>
+                {/* <Route path="./Conponenets/Login/Login.js" element={<Login />}/>
+                <Route path="./Conponenets/Sign_Up/Sign_Up.js" element={<SignUp />}/>
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
                  <Route path="<component_route>" element={<component_name/>}/> //replace the component_route with the component path  and component_name with the component name as imported in the **App.js file**. 
-                />
+                 */}
               </Routes>
           </Notification>
         </BrowserRouter>
+        </div>
+  );
+}
+
+export default App;
